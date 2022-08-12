fa8bf24deb33bb110efa486309307d46123f2cc5
azrgod <azraelsuperfire@gmail.com>
2022-08-12 00:40:57 -0600

    Initial commit

---
+++ src/main/java/sv/edu/ufg/webservices/webservicepersonas/entities/dto/PersonasDTO.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sv.edu.ufg.webservices.webservicepersonas.entities.dto;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author diego
+ */
+public class PersonasDTO implements Serializable {
+    private String nombres;
+    private String apellidos;
+    private String sexo;
+    private String email;
+    private Date fechadenacimiento;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFechadenacimiento() {
+        return fechadenacimiento;
+    }
+
+    public void setFechadenacimiento(Date fechadenacimiento) {
+        this.fechadenacimiento = fechadenacimiento;
+    }
+    
+    
+}
